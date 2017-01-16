6ba5c9d268863ec0e03bcf39e33b1b492c5c9875
JonMcCarthy7 <jtmcc93@gmail.com>
2017-01-16 15:56:15 -0700

    add image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  # This method associates the attribute ":avatar" with a file attachment
+ has_attached_file :avatar, styles: {
+   thumb: '100x100>',
+   square: '200x200#',
+   medium: '300x300>',
+   large: '600x600>'
+ }
+
+ # Validate the attached image is image/jpg, image/png, etc
+ validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
--- db/migrate/20170113194041_add_attachment_avatar_to_users.rb
+++ db/migrate/20170113194041_add_attachment_avatar_to_users.rb
@@ -1,11 +1,23 @@
 class AddAttachmentAvatarToUsers < ActiveRecord::Migration
-  def self.up
-    change_table :users do |t|
-      t.attachment :avatar
-    end
-  end
 
-  def self.down
-    remove_attachment :users, :avatar
-  end
+    def change
+
+      add_attachment :users, :avatar
+
+    end
+
+
+
+
+
+
+  # def self.up
+  #   change_table :users do |t|
+  #     t.attachment :avatar
+  #   end
+  # end
+  #
+  # def self.down
+  #   remove_attachment :users, :avatar
+  # end
 end
